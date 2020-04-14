b0f995e149c21a52674e238f68664f87d6b0236f
nadimgkmit <nadim@gkmit.co>
2020-04-14 15:35:43 +0530

    create login form [#172242211]

--- src/controller/login.jsx
+++ src/controller/login.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
@@ -14,6 +10,7 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
 import Copyright from '../component/copyright'
+import MuiForm from '../component/form'
 
 const styles = (theme) => ({
     root: {
@@ -36,18 +33,54 @@ const styles = (theme) => ({
     avatar: {
         margin: theme.spacing(1),
         backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+    }
 });
 
 
 class Login extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            form: [
+                {
+                    name: 'email',
+                    label: 'email',
+                    type: 'email',
+                    icon: 'mail',
+                    value: '',
+                    validation: 'required|email',
+                },
+                {
+                    name: 'password',
+                    label: 'password',
+                    type: 'password',
+                    icon: 'password',
+                    value: '',
+                    validation: 'required',
+                },
+                {
+                    name: 'remember_me',
+                    label: 'Remember me',
+                    type: 'checkbox',
+                    value: '',
+                    validation: 'required',
+                },
+            ]
+        }
+    }
+
+    handleChange = (value, index) => {
+        const { form } = this.state
+        form[index].value = value
+        this.setState(form)
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.history.push('/admin')
+    }
+
     render() {
         const { classes } = this.props
 
@@ -63,58 +96,31 @@ class Login extends React.Component {
                         <Typography component="h1" variant="h5">
                             Sign in
                         </Typography>
-                        <form className={classes.form} noValidate>
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                            />
-                            <TextField
-                                variant="outlined"
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                className={classes.submit}
-                            >
-                                Sign In
-                </Button>
-                            <Grid container>
-                                <Grid item xs>
-                                    <Link href="#" variant="body2">
-                                        Forgot password?
-                    </Link>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="#" variant="body2">
-                                        {"Don't have an account? Sign Up"}
-                                    </Link>
-                                </Grid>
+
+                        <MuiForm
+                            formFields={this.state.form}
+                            handleChange={this.handleChange}
+                            handleSubmit={this.handleSubmit}
+                            submitText="Sign In"
+                            submitFullWidth={true}
+                            noValidate={false}
+                        />
+
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
                             </Grid>
-                            <Box mt={5}>
-                                <Copyright />
-                            </Box>
-                        </form>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                        <Box mt={5}>
+                            <Copyright />
+                        </Box>
                     </div>
                 </Grid>
             </Grid>
