ba60a2a9caebcd777b9d5124cbfe11b93d969180
nadimgkmit <nadim@gkmit.co>
2020-04-17 11:26:15 +0530

    add loader component [#172242223]

--- src/app.jsx
+++ src/app.jsx
@@ -2,18 +2,50 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './pages/login';
 import AdminRoutes from './adminRoutes';
+import { connect } from 'react-redux';
+import { loaderActions } from './_actions';
 
+import Loader from './component/alert/loader';
 class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    componentWillMount() {
+        this.props.showLoader();
+    }
+
+    componentDidMount() {
+        this.props.hideLoader();
+    }
+
     render() {
         return (
-            <Router>
-                <Switch>
-                    <Route path='/' exact component={Login} />
-                    <AdminRoutes />
-                </Switch>
-            </Router>
+            <React.Fragment>
+
+                <Loader open={this.props.loader} />
+
+                <Router>
+                    <Switch>
+                        <Route path='/' exact component={Login} />
+                        <AdminRoutes />
+                    </Switch>
+                </Router>
+            </React.Fragment>
         )
     }
 }
 
-export default App;
+
+function mapState(state) {
+    const { loader } = state;
+    return { loader };
+}
+
+const actionCreators = {
+    showLoader: loaderActions.show,
+    hideLoader: loaderActions.hide,
+};
+
+export default (connect(mapState, actionCreators)(App));
